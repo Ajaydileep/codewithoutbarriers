1a0481ad999ef5068994a06724613adc1d40f64f
Ajay dileep <137861722+Ajaydileep@users.noreply.github.com>
2024-10-10 13:16:13 +0530

    Add files via upload

---
+++ cwb.js
@@ -0,0 +1,13 @@
+// document.getElementById('register-btn').addEventListener('click', function() {
+//     // Redirect to the registration page URL
+//     window.location.href = 'Registration.html';  // Update this with the correct path to your registration page
+// });
+
+// document.getElementById('register-btn').onclick = function() {
+//     window.location.href = 'Registration.html';
+// };
+
+document.getElementById('register-btn').onclick = function() {
+    console.log('Button clicked!');
+    window.location.href = 'Registration.html';
+};
